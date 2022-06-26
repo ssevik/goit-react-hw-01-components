5392eef1443625cc731fa72a25404b23b1471465
Artem <talibanssevik@gmail.com>
2022-06-26 21:59:46 +0300

    fix7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
-import { Profile } from 'components/Profile/Profile';
+import { Container } from './common/Container.styled';
+import { Profile } from './Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-
-import { Container } from './common/Container.styled';
 
 import users from '../Data/user.json';
 import data from '../Data/data.json';
