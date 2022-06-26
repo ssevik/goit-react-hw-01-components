5fc944315b690d8f561430e86374fdc13d92ad92
Artem <talibanssevik@gmail.com>
2022-06-26 21:24:39 +0300

    fix5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
-import { Container } from 'components/common/Container.styled';
+// import { Container } from 'components/common/Container.styled';
 
 import users from 'Data/user.json';
 import data from 'Data/data.json';
@@ -13,20 +13,18 @@ import transactions from 'Data/transactions.json';
 export const App = () => {
   return (
     <>
-      <Container>
-        <Profile
-          src={users.avatar}
-          name={users.username}
-          tag={users.tag}
-          location={users.location}
-          likes={users.stats.likes}
-          views={users.stats.views}
-          followers={users.stats.followers}
-        />
-        <Statistics title="Upload stats" stats={data} />
-        <FriendList friends={friend} />
-        <TransactionHistory items={transactions} />
-      </Container>
+      <Profile
+        src={users.avatar}
+        name={users.username}
+        tag={users.tag}
+        location={users.location}
+        likes={users.stats.likes}
+        views={users.stats.views}
+        followers={users.stats.followers}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friend} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
