ce09d5485873a8905fc124caea96a9a80935b65b
Artem <talibanssevik@gmail.com>
2022-06-26 21:52:02 +0300

    fix6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,30 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
-// import { Container } from 'components/common/Container.styled';
+import { Container } from './common/Container.styled';
 
-import users from 'Data/user.json';
-import data from 'Data/data.json';
-import friend from 'Data/friends.json';
-import transactions from 'Data/transactions.json';
+import users from '../Data/user.json';
+import data from '../Data/data.json';
+import friend from '../Data/friends.json';
+import transactions from '../Data/transactions.json';
 
 export const App = () => {
   return (
     <>
-      <Profile
-        src={users.avatar}
-        name={users.username}
-        tag={users.tag}
-        location={users.location}
-        likes={users.stats.likes}
-        views={users.stats.views}
-        followers={users.stats.followers}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friend} />
-      <TransactionHistory items={transactions} />
+      <Container>
+        <Profile
+          src={users.avatar}
+          name={users.username}
+          tag={users.tag}
+          location={users.location}
+          likes={users.stats.likes}
+          views={users.stats.views}
+          followers={users.stats.followers}
+        />
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friend} />
+        <TransactionHistory items={transactions} />
+      </Container>
     </>
   );
 };
