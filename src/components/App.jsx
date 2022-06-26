3db35413a83b091df397dc05a33bedfe50ae2e45
Artem <talibanssevik@gmail.com>
2022-06-26 22:49:34 +0300

    fix19

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,31 @@
-import { Profile } from 'profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import { Container } from './common/Container.styled';
+import { Profile } from './profile/Profile';
+import { Statistics } from './components/Statistics/Statistics';
+import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
 
-import users from 'Data/user.json';
-import data from 'Data/data.json';
-import friend from 'Data/friends.json';
-import transactions from 'Data/transactions.json';
+import users from '../Data/user.json';
+import data from '../Data/data.json';
+import friend from '../Data/friends.json';
+import transactions from '../Data/transactions.json';
 
 export const App = () => {
   return (
     <>
-      <Profile
-        src={users.avatar}
-        name={users.username}
-        tag={users.tag}
-        location={users.location}
-        likes={users.stats.likes}
-        views={users.stats.views}
-        followers={users.stats.followers}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friend} />
-      <TransactionHistory items={transactions} />
+      <Container>
+        <Profile
+          src={users.avatar}
+          name={users.username}
+          tag={users.tag}
+          location={users.location}
+          likes={users.stats.likes}
+          views={users.stats.views}
+          followers={users.stats.followers}
+        />
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friend} />
+        <TransactionHistory items={transactions} />
+      </Container>
     </>
   );
 };
