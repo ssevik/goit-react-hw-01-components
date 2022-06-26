f1ec7a891a3223a5a18112d1748c0bb035b1bf43
Artem <talibanssevik@gmail.com>
2022-06-26 22:42:25 +0300

    fix16

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import { Container } from './common/Container.styled';
-import { Profile } from './profile/Profile';
+import { Container } from 'common/Container.styled';
+import { Profile } from 'profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from './components/FriendList/FriendList';
-import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
-import users from '../Data/user.json';
-import data from '../Data/data.json';
-import friend from '../Data/friends.json';
-import transactions from '../Data/transactions.json';
+import users from 'Data/user.json';
+import data from 'Data/data.json';
+import friend from 'Data/friends.json';
+import transactions from 'Data/transactions.json';
 
 export const App = () => {
   return (
